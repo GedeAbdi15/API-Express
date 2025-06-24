ba508fd9a5efb488283b17203208aba816734721
GedeAbdi15 <abdi.wicaksana58@gmail.com>
2025-06-24 22:45:31 +0700

    Fix bug endpoint user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -14,8 +14,9 @@ function generateUID() {
 }
 
 // users : method get
-exports.getAllUsers = (req, res) => {
-    const sql = "SELECT * FROM users";
+exports.getAllUsers = (res) => {
+    const sql =
+        "SELECT `users`.`id`, `uid`, `roles`.`id` AS role_id, `roles`.`role`, `users`.`name`, `users`.`email`, `users`.`password`, `users`.`phone_number`, `users`.`created_at`, `users`.`updated_at`, `users`.`deleted_at` FROM `users` LEFT JOIN `roles` ON `roles`.`id` = `users`.`role` ";
 
     db.query(sql, (err, results) => {
         if (err) {
@@ -32,27 +33,40 @@ exports.getAllUsers = (req, res) => {
                 users: results,
             },
         });
+
+        // console.log("debug : ", res);
     });
 };
 
 // users : method post
 exports.createUser = async (req, res) => {
-    const { name, email, password, role, phone } = req.body;
+    const { name, email, password, role, phone_number } = req.body;
 
-    if (!name || !email) {
+    if (!name) {
         return res.status(400).json({
             success: false,
-            message: "Name and email are required",
+            message: "Name is required",
         });
     }
 
     try {
         const uid = generateUID();
-        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        const encryptedPassword = null;
+        if (password && password.trim() != "") {
+            encryptedPassword = await bcrypt.hash(password, 10);
+        }
 
         const sql =
             "INSERT INTO users (uid, name, email, password, role, phone_number) VALUES (?, ?, ?, ?, ?, ?)";
-        const val = [uid, name, email, encryptedPassword, role, phone];
+        const val = [
+            uid,
+            name,
+            email || null,
+            encryptedPassword,
+            role,
+            phone_number || null,
+        ];
 
         db.query(sql, val, (err, result) => {
             if (err) {
@@ -66,13 +80,8 @@ exports.createUser = async (req, res) => {
             res.status(201).json({
                 success: true,
                 message: "User added successfully",
-                user: {
-                    uid,
-                    role,
-                    name,
-                    email,
-                    password,
-                    phone,
+                data: {
+                    user: result,
                 },
             });
         });
@@ -88,14 +97,24 @@ exports.createUser = async (req, res) => {
 // users : method put
 exports.updateUser = async (req, res) => {
     const id = req.params.id;
-    const { role, name, email, password, phone } = req.body;
+    const { role_id, name, email, password, phone_number } = req.body;
 
     try {
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = null;
+        if (password && password.trim() != "") {
+            hashedPassword = await bcrypt.hash(password, 10);
+        }
 
         const sql =
             "UPDATE `users` SET `role`= ? , `name`= ? , `email`= ? , `password`= ?, `phone_number` = ?  WHERE `id` = ?";
-        const val = [role, name, email, hashedPassword, phone, id];
+        const val = [
+            role_id,
+            name,
+            email || null,
+            hashedPassword,
+            phone_number || null,
+            id,
+        ];
 
         db.query(sql, val, (err, result) => {
             if (err) {
@@ -109,12 +128,8 @@ exports.updateUser = async (req, res) => {
             res.status(200).json({
                 success: true,
                 message: "User updated successfully",
-                user: {
-                    id,
-                    role,
-                    name,
-                    email,
-                    phone,
+                data: {
+                    user: result,
                 },
             });
         });
@@ -130,9 +145,9 @@ exports.updateUser = async (req, res) => {
 // users : mehod delete
 exports.deleteUser = (req, res) => {
     const id = req.params.id;
-    const sql = "DELETE FROM `users` WHERE `id` = '?'";
+    const sql = "DELETE FROM `users` WHERE `id` = ?";
 
-    db.query(sql, [id], (err, result) => {
+    db.query(sql, [id], (err) => {
         if (err) {
             console.error("Failed to delete user with id cause", err);
             return res.status(500).json({
